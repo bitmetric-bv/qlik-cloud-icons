d17d280d572a61b072a116dcaa218dd82f6fd1f6
Barry Harmsen <barry@bitmetric.nl>
2024-09-10 18:05:17 +0200

    Added new icons

--- create-readme.py
+++ create-readme.py
@@ -1,14 +1,17 @@
 import os
 
-# Initialize README.md
-f = open("README.md", "w")
-f.write("# qlik-cloud-icons\nCollection of Qlik Cloud icons in SVG format\n\n|Filename|Preview|\n|---|---|\n")
-
 # Get list of SVGs
 items = os.listdir('./svg')
 sorted_items = sorted(items)
 
-# Loop & write
+# Initialize README.md
+f = open("README.md", "w")
+f.write("# qlik-cloud-icons\nCollection of " + str(len(sorted_items)) + " Qlik Cloud icons in SVG format.\n\n")
+
+# Header
+f.write("|Filename|Preview|\n|---|---|\n")
+
+# Loop & write rows
 for item in sorted_items:
     f.write("|" + item + "|<img src='./svg/" + item + "' width='32' height='32'/>|\n")
 
